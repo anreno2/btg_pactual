6ddff493c07b45f5a10dc0ee1f246b8f28adf074
Carlos Villa <carlos.villarojas@externos-cl.cencosud.com>
2026-03-05 00:15:30 -0500

    sms and email

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://3.148.217.12:3000/api"
-  //baseURL: "http://localhost:3000/api"
+  //baseURL: "http://3.148.217.12:3000/api"
+  baseURL: "http://localhost:3000/api"
 });
 
 api.interceptors.request.use((config) => {
